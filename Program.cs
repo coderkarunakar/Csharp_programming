a91858c1e7d4536282520259415ebd680233d359
Chembeti Karunakar <chembeti.61095916@ltimindtree.com>
2023-09-15 11:46:36 +0530

    data Type conversion

--- Program.cs
+++ Program.cs
@@ -8,81 +8,44 @@ namespace consoleApp2
     //this class allows us to model import object etc
     public class Program
     {
-        //all our code is start executing in our main code
-        // static means it allows us to run with out creating an objects and void means this our function called main is not going to return any values
-        //string array which contains bunch of data inside it 
-        //any data i.e passed to args executes in a terminal window 
-
+       
         static void Main(string[] args)
         {
-            //variables:stores some specif type of data
-            //bool can have a true or a false
-            bool canIVote = true;
+            //Type casting :converting data types using Parse(we can convert)
+            //converting string to bool
+            bool boolFromStr = bool.Parse("true");
 
-            //integers it can have 32 bit or 64 bit,here we can use $ or {} as well,and MaxValue gives you the biggest integer in integers 
-            Console.WriteLine("Biggest Integer:{0}", int.MaxValue);
+            //string to int
+            int intFromStr = int.Parse("100");
+            //string to double
+            double dblFromStr = double.Parse("1.234"); 
 
-            //smallest integer
-            Console.WriteLine("Smallest Integer:{0}", int.MinValue);
-
-            //it is going to store 64 bit
-            Console.WriteLine("Biggest Long:{0}", long.MaxValue);
-
-            Console.WriteLine("Smallest Long:{0}", long.MinValue);
-
-            decimal decPiVal = 3.1415M;
-
-            //bignum is going to store 128 bit
-            decimal decBigNum = 3.000M;
-            Console.WriteLine("DEC:PI+bigNum={0}",decPiVal+decBigNum);
+            //double into a  string (here ToString is used to convert any datatype to string
+            string strVal =dblFromStr.ToString();
 
 
-
-            Console.WriteLine("Biggest Decimal:{0}", Decimal.MaxValue);
-
-            Console.WriteLine("Bigggest Double:{0}", Double.MaxValue);
-
-            double dblPiVal = 3.1415;
-            double dblBigNum = 3.1415;
-            Console.WriteLine("DBL:PI+bigNum={0}", dblPiVal+dblBigNum);
+            //here {} is used to get the data fetch from the defined
+            Console.WriteLine($"Data type:{strVal.GetType()}");
 
 
-            Console.WriteLine("Bigggest Float:{0}", float.MaxValue);
+            //Explicity convertion (loosing some data)
 
-            double fltPiVal = 3.1415F;
-            double fltBigNum = 3.0002F;
-            Console.WriteLine("FLT:PI+bigNum={0}", fltPiVal + fltBigNum);
+            //convert double into integer(here we loose all of the decimal value )
+            double dblNum = 12.345;
 
+            Console.WriteLine($"Integer:{(int)dblNum}");
 
-
-            //output for the above code is 
-            //Biggest Integer:2147483647
-//Smallest Integer:-2147483648
-//Biggest Long:9223372036854775807
-//Smallest Long:-9223372036854775808
-//DEC: PI + bigNum = 6.1415
-//Biggest Decimal:79228162514264337593543950335
-//Bigggest Double:1.7976931348623157E+308
-//DBL: PI + bigNum = 6.283
-//Bigggest Float:3.4028235E+38
-//FLT: PI + bigNum = 6.141700029373169
+            //implicit conversion  :here we convert all the data types into the larger data type
+            int intNum = 10;
+            long longNum = intNum;
+            Console.WriteLine(longNum);
 
 
+            //output for the above code:
+                /*Data type:System.String
+                Integer:12(here we loose the data)
+                10*/
 
-
-
-            /*other different data types are as follows
-            *byte:8-bit unsigned int 0 to 255
-            *char:16-bit unicode character 
-            *sbyte:8-bit signed int 128 to 127
-            *short:16-bit signed int-32,768 to 32,767
-            *uint:32-bit unsigned int 0 to 4,294,967,295
-            *ulong:64bit unsigned int 0 to  *18,446,744,073,709,551,615
-            *ushort:16-bit unsigned int 0 to 65,535*/
-
-
-            //NOTE:
-            //difference between console.write(prints without new line) and console.writeline(prints with new line) 
 
 
 
