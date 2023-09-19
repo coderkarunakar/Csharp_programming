a3d56fa2db7812a07e384b8c6e363094bbfb7a2c
Chembeti Karunakar <chembeti.61095916@ltimindtree.com>
2023-09-19 09:50:26 +0530

    String Functions,Format,Escape Characters,Verbatim Strings

--- Program.cs
+++ Program.cs
@@ -11,23 +11,92 @@ namespace consoleApp2
        
         static void Main(string[] args)
         {
-            //Formating output
-            Console.WriteLine("Currency:{0:c}",23.455666);
 
-            //here d4 means it will make it as 4 numbers by adding zeroes at the front   
-            Console.WriteLine("Pad with 0s:{0:d4}", 23);
-            //3decimals(it is going to  give u the 3 decimals)
-            Console.WriteLine("3 decimals:{0:f3}", 23.45555);
-            //it is goint to give  you 4 zeroes after this number decimal...
-            Console.WriteLine("Commas:{0:n4}", 2300);
+            //String
+            //Note:this whole string which is in double quote is reffered as index 0 i.e {0}
+
+            string randString = "This is a String";
+
+            //get no of character in the string
+            Console.WriteLine("String length:{0}", randString.Length);
+
+            //it is going to return true or false
+            Console.WriteLine("String contains is :{0}", randString.Contains("is"));
 
 
-            //output for the above code
+            //get the idex of a specif character in the given string 
+            Console.WriteLine("Index of is:{0}", randString.IndexOf("is"));
 
-       /* Currency:? 23.45
-            Pad with 0s: 0023
-            3 decimals: 23.456
-            Commas: 2,300.0000*/
+            //remove the no of character from starting and a specif index
+            Console.WriteLine("String length:{0}", randString.Length);
+
+            //remove string
+            Console.WriteLine("Remove String :{0}", randString.Remove(10,6));
+
+            //insert string
+            Console.WriteLine("Insert String :{0}", randString.Insert(10,"short"));
+
+            //replace a string
+            Console.WriteLine("Replace String:{0}", randString.Replace("string","sentence"));
+
+            //Compare a string
+            Console.WriteLine("Compare A to B:{0}", String.Compare("A","B",
+                StringComparison.OrdinalIgnoreCase));
+
+
+            //bit confusion here
+            //compare strings and ignore case
+            //<0:str1 preceeds str2(above is this case)
+            //=:Zero
+            //>0:str2 preceeds str1
+
+            Console.WriteLine("----------");  
+            
+            //comparing uppercase A and lower case a
+           Console.WriteLine("A=a:{0}",String.Equals("A","a",
+               StringComparison.OrdinalIgnoreCase));
+
+            //Add padding to a string
+            //this will add a padding with dots at the left side
+            Console.WriteLine("Pad Left:{0}",
+                randString.PadLeft(20,'.'));
+
+            //Adding padding to the right
+            //this will add a padding to the right side with dots
+            Console.WriteLine("Pad Right:{0}",
+                randString.PadRight(20, '.'));
+
+            //Trim (i.e white space
+            //it removes all the leading and trailing white spaces
+            Console.WriteLine("Trim:{0}",
+                randString.Trim());
+
+
+            Console.WriteLine("UpperCase:{0}",
+                randString.ToUpper());
+
+            Console.WriteLine("LowerCase:{0}",
+                randString.ToLower());
+
+            //Formating the string
+            //Note:{0} means whole string which is in double quotes
+            string newstring = string.Format("{0} saw a {1} {2} in the {3}",
+                "Paul", "rabbit", "eating", "field");
+
+
+            //adding the new line to the string
+            Console.Write(newstring + "\n");
+
+            //to use a double quote inside the string
+            //  \'\"\\ \t \a
+            Console.WriteLine(@"Exactly what I typed\n");
+
+
+
+
+
+
+
 
         }
     }
