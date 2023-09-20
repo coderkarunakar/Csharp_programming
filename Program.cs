456da382fd40c8d47d1c80fba2a964f313de28e2
Chembeti Karunakar <chembeti.61095916@ltimindtree.com>
2023-09-20 10:30:47 +0530

    Function basics and ForEach loop

--- Program.cs
+++ Program.cs
@@ -8,7 +8,22 @@ namespace consoleApp2
     //this class allows us to model import object etc
     public class Program
     {
-       
+        //---Functions
+        //
+
+        //static means we do not need to create a new object
+        //here name of the function is PrintArray
+        static void PrintArray(int[] intArray,string mess)
+        {
+            foreach(int k in intArray)
+            {
+                Console.WriteLine("{0}:{1}",mess,k);
+            }
+        }
+
+
+
+        //---------End of Functions--
         static void Main(string[] args)
         {
             //Topic :Arrays and For loops
@@ -31,17 +46,17 @@ namespace consoleApp2
             var employees = new[] { "Mike", "Paul", "Rick" };
 
             //creating array using objects
-            object[] randomArray = { "Paul", 45, 1.234,"karunakar" };
+            object[] randomArray = { "Paul", 45, 1.234, "karunakar" };
             Console.WriteLine("randomArray 0:{0}",
                 randomArray[0].GetType());
             Console.WriteLine("Array Size:{0}", randomArray.Length);
 
             //using a for loop for generating a index value
-            for(int j=0;j<randomArray.Length;j++)
+            for (int j = 0; j < randomArray.Length; j++)
             {
                 //for generating index value i have used j
                 //here Array {0} is fetching its index and value is fetching value which starts with 1 and that goes on...
-                Console.WriteLine("array:{0}:value:{1}",j, randomArray[j]);
+                Console.WriteLine("array:{0}:value:{1}", j, randomArray[j]);
             }
             Console.WriteLine("---------");
 
@@ -54,30 +69,113 @@ namespace consoleApp2
                         /*column0*/ /*column1*/
               /*row0*/  {"Bob","Smaith"},
                /*row1*/ {"Sally","Smith" }
-                
+
             };
             //get a value inside the multidimensional array using indexes
-            Console.WriteLine("MD Value:{0}", 
-                //here the value is about row(first) and column(second) 
-                //GetValue is used to get the value of specified element in the current Array
-            custNames.GetValue(1,1));
-                
-            for(int j = 0; j < custNames.GetLength(0); j++)
+            Console.WriteLine("MD Value:{0}",
+            //here the value is about row(first) and column(second) 
+            //GetValue is used to get the value of specified element in the current Array
+            custNames.GetValue(1, 1));
+
+            for (int j = 0; j < custNames.GetLength(0); j++)
             {
-                for(int k = 0; k < custNames.GetLength(0); k++)
+                for (int k = 0; k < custNames.GetLength(0); k++)
                 {
-                    Console.WriteLine("{0}", custNames[j,k]);
+                    Console.WriteLine("{0}", custNames[j, k]);
 
                 }
                 //for empty space
                 Console.WriteLine();
             }
+            int[] randNums = { 1, 4, 9, 2,8,100 };
+
+            //function name and passing an array and the message is ForEach and the iteration is taking from the above randNums
+            PrintArray(randNums, "ForEach");
+            Console.WriteLine("------------");
+
+            //Sorting an  array
+            Array.Sort(randNums);
+
+            //Reverse an array
+            Array.Reverse(randNums);
+
+            //getting an specified index value of an array
+            //here actually that {0} is fetching to 1 which is in randNums
+            Console.WriteLine("1 at index:{0}",
+                Array.IndexOf(randNums, 1));
+
+
+            //Change the value at an index
+            randNums.SetValue(0, 1);
+
+
+            //copy an array to an another part of an array
+
+            //source array
+            int[] srcArray = { 1, 2, 3 };
+            //destination array
+            int[] destArray = new int[2];
+            int startInd = 0;
+            int length = 2;
+            //with this i can copy elements from 0 to till upto lenght 2
+            Array.Copy(srcArray,startInd, destArray,0,length);
+            PrintArray(destArray, "Copy");
+
+            //here 10 is length
+            Array anotherArray=Array.CreateInstance(typeof(int), 10);
+
+            //here startInd is 5
+            srcArray.CopyTo(anotherArray, 5);
+            foreach(int m in anotherArray)
+            {
+                Console.WriteLine("CopyTo:{0}", m);
+
+            }
+
+                /*favNum 0:23
+                randomArray 0:System.String
+                Array Size:4
+                array:0:value:Paul
+                array:1:value:45
+                array:2:value:1.234
+                array:3:value:karunakar
+                ---------
+                MD Value:Smith
+                Bob
+                Smaith
+
+                Sally
+                Smith
+
+                ForEach:1
+                ForEach:4
+                ForEach:9
+                ForEach:2
+                ForEach:8
+                ForEach:100
+                ------------
+                1 at index:5
+                Copy:1
+                Copy:2
+                CopyTo:0
+                CopyTo:0
+                CopyTo:0
+                CopyTo:0
+                CopyTo:0
+                CopyTo:1
+                CopyTo:2
+                CopyTo:3
+                CopyTo:0
+                CopyTo:0
+                */
+
+
+
+
+
+               
 
            
-
-
-
-
 
 
 
