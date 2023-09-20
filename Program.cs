e29647c8f6d8ea414521d72e86876deff949c21d
Chembeti Karunakar <chembeti.61095916@ltimindtree.com>
2023-09-20 12:07:11 +0530

    terinary operator, switch statement, default,go to

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿//here we are just importing the System
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using static System.Net.Mime.MediaTypeNames;
+using System.Dynamic;
 
 //working with name space
 namespace consoleApp2
@@ -15,37 +18,59 @@ namespace consoleApp2
             //if else and else if
             //in conditional opertors we have 2 operators i.e realational operator:> < >= <= ==  != and in the logical operators:&& || !
 
-            int age = 17;
-            if((age>=5) &&(age<=7))
+            int age = 1;
+           
+
+            //Terinay operator:it is going to check the given condition is true or false ,by checking with the question mark and gives true or false since we have used bool
+            bool canDrive = age >= 16 ? true : false;
+            Console.WriteLine(canDrive);
+
+
+
+            //Note:default get executed only when all the cases are not matching only ,and if once break is kept and case is matching only that case  will get executed and no default get executes vv.imp
+
+            switch(age)
             {
-                Console.WriteLine("Go to elementary School");
+                case 1:
+                case 2:
+                    Console.WriteLine("Go to Day care");
+                    //Note:this break brings out of the switch statement and wont check other statements
+                    break;
+
+                case 3:
+                case 4:
+                    Console.WriteLine("Go to Preschool");
+                  
+                    break;
+                case 5:
+                    Console.WriteLine("Go to Kindergarten");
+                    break;
+
+                default:
+                    Console.WriteLine("Go to another School");
+                    //we can jump from switch statement by using go to,Even though default get executed or case statement get executed the goto will definetely work in all the cases 
+                    goto OtherSchool;
+
+
+
             }
-            else if ((age >7) && (age <13))
+        OtherSchool:
+            Console.WriteLine("Elementary,Middle,High School");
+
+
+
+
+            string name2 = "Derek";
+            string name3 = "Derek"; 
+            if(name2.Equals(name3,StringComparison.Ordinal))
             {
-                Console.WriteLine("Go to Middle School");
+                Console.WriteLine("Names are Equal");
             }
-            else if ((age > 13) && (age < 19))
-            {
-                Console.WriteLine("Go to High School");
-            }
-            else {
-                Console.WriteLine("Go to College");
-            }
-            if ((age <14) && (age < 67))
-            {
-                Console.WriteLine("To should not work");
-            }
-            Console.WriteLine("! true =" +(!true));
 
-
-            //here we can use if statement else statement and instead of using if statement 2nd time we can use else if it is similar to elif which is in python
-
-
-
-
-
-
-
+            /*StringComparison.Ordinal:
+             * lets you test for equality by calling either the static or instance Equals method overloads, or by using the static equality operator
+             
+            .Equals:used to determine whether two String objects have the same value or not. */
 
 
 
